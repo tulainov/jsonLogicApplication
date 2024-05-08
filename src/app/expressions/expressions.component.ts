7ee5f2d3f83b61527c620a387e8e9643a8b57f7c
Danylo Tulainov <danylo.tulainov@voith.com>
2024-05-08 07:32:04 +0200

    The logic itself finally works. I'm working on styles now.

--- src/app/expressions/expressions.component.ts
+++ src/app/expressions/expressions.component.ts
@@ -11,33 +11,64 @@ import * as jsonLogic from 'json-logic-js';
 export class ExpressionsComponent {
 
   operationList = new OperationsList();
-  selectedOperation: string = '';
+  selectedOperation: string = ''; // simple operations
+  selectedNodeOperation: string = ''; // operations between nodes
   firstInput?: any;
   secondInput?: any;
   result: any;
-  treeData: any[] = [];
+  treeData: any[] = []; // represents decision tree
+  results: any[] = []; // stores results of simple operations
+  nodeResult: any; // final result of node operations
 
-
+  // selects simple and node operation
   public selectOperation(value: any) {
-    console.log("value", value);
+    if (value.target.id === 'simpleOperation') {
+      this.selectedOperation = value.target.value;
+    } else if (value.target.id === 'nodesOperation') {
+      this.selectedNodeOperation = value.target.value;
+    }
   }
 
+  // generates result for simple operations
   public generateResult() {
 
     if (this.firstInput !== undefined && this.firstInput !== null &&
       this.secondInput !== undefined && this.secondInput !== null) {
+
       this.result = jsonLogic.apply({
-        [this.selectedOperation]:
-          [this.firstInput, this.secondInput]
+        [this.selectedOperation] : [this.firstInput, this.secondInput]
       });
+
+      this.results.push(this.result);
       this.addNodeToTree();
     }
   }
 
+  // generates result for node operations
+  public completeNodeOperation() {
+
+    if (this.results.length >= 2) {
+      this.nodeResult = jsonLogic.apply({
+        [this.selectedNodeOperation]: this.results
+      })
+    // } else {
+    //   for (let i = 0; i < this.results.length; i++) {
+    //     this.firstResult = this.results[0];
+    //     this.secondResult = this.results[1];
+    //
+    //     this.nodeResult = jsonLogic.apply({
+    //       [this.selectedNodeOperation]: [this.firstResult, this.secondResult]
+    //     });
+    //   }
+    }
+  }
+
+  // adds node to tree
   private addNodeToTree() {
+
     this.treeData.push({
-      operation: this.selectedOperation,
       firstInput: this.firstInput,
+      operation: this.selectedOperation,
       secondInput: this.secondInput,
       result: this.result
     });
@@ -47,4 +78,13 @@ export class ExpressionsComponent {
     this.treeData = [];
   }
 
+  public refreshNodeResult() {
+    this.nodeResult = '';
+  }
+
+
+  public refresh() {
+    location.reload();
+  }
 }
+
